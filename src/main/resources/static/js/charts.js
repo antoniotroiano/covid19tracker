9f2a7dc1f03111b87bbc4d93922093f21f324f8c
Antonio Troiano <antonio.troiano@valtech.com>
2020-05-06 22:15:24 +0200

    All pages are more responsive. Add line chart and stacked bar chart to world and germany data.

--- src/main/resources/static/js/charts.js
+++ src/main/resources/static/js/charts.js
@@ -54,24 +54,58 @@ function openModal(region, confirmed, recovered, deaths, active) {
     });
 }
 
+let backgroundColor = 'rgb(147,190,234, 0.6)';
+let borderColor = 'rgb(13,23,234)';
+let hoverBackgroundColor = 'rgb(147,190,234, 0.7)';
+let typeChart = 'bar';
+let booleanStacked = true;
+let barChart;
+
+/*Toggle between bar chart*/
+function changeBarChart(canvas, dates, label, listConfirmed, listRecovered, listDeaths, type) {
+    barChart.destroy();
+
+    if (label === 'Recovered') {
+        backgroundColor = 'rgba(167,234,122,0.6)';
+        borderColor = 'rgb(9,234,14)';
+        hoverBackgroundColor = 'rgba(187,234,109,0.7)';
+        barChartSelectedCountry(canvas, dates, label, listConfirmed);
+    } else if (label === 'Deaths') {
+        backgroundColor = 'rgba(234,127,121,0.6)';
+        borderColor = 'rgb(234,3,12)';
+        hoverBackgroundColor = 'rgba(234,81,77,0.7)';
+        barChartSelectedCountry(canvas, dates, label, listRecovered);
+    } else if (label === 'Confirmed') {
+        backgroundColor = 'rgb(147,190,234, 0.6)';
+        borderColor = 'rgb(13,23,234)';
+        hoverBackgroundColor = 'rgb(147,190,234, 0.7)';
+        barChartSelectedCountry(canvas, dates, label, listDeaths);
+    } else if (type === 'bar') {
+        typeChart = 'bar';
+        booleanStacked = true;
+        barChartAll(canvas, dates, label, listConfirmed, listRecovered, listDeaths);
+    } else {
+        typeChart = 'line';
+        booleanStacked = false;
+        barChartAll(canvas, dates, label, listConfirmed, listRecovered, listDeaths)
+    }
+}
+
 /*Bar chart for selected country*/
-function barChartSelectedCountry(confCanvas, recCanvas, deaCanvas, confLabel, recLabel, deaLabel, datesA, confList, recList, deaList) {
-    const dates = datesA;
-    const ctxConfirmed = confCanvas.getContext('2d');
-    const dataConfirmed = new Chart(ctxConfirmed, {
+function barChartSelectedCountry(canvas, dates, label, listData) {
+    barChart = new Chart(canvas, {
         type: 'bar',
         data: {
             labels: dates,
             datasets: [{
-                label: confLabel,
-                backgroundColor: 'rgb(147,190,234, 0.6)',
+                label: label,
+                backgroundColor: backgroundColor,
                 borderWidth: 1,
-                borderColor: 'rgb(13,23,234)',
-                hoverBackgroundColor: 'rgb(147,190,234, 0.7)',
-                barPercentage: 0.9,
-                barThickness: 10,
-                maxBarThickness: 15,
-                data: confList
+                borderColor: borderColor,
+                hoverBackgroundColor: hoverBackgroundColor,
+                barThickness: 8,
+                maxBarThickness: 10,
+                data: listData
             }]
         },
         options: {
@@ -88,7 +122,7 @@ function barChartSelectedCountry(confCanvas, recCanvas, deaCanvas, confLabel, re
                     },
                     ticks: {
                         autoSkip: true,
-                        maxTicksLimit: 25
+                        maxTicksLimit: 17
                     }
                     /*callback: function (dataLabel) {
                         dateDeath.push(dataLabel);
@@ -97,9 +131,9 @@ function barChartSelectedCountry(confCanvas, recCanvas, deaCanvas, confLabel, re
                         } else {
                             return dataLabel + '.2020';
                         }
-                    }*/
-                    // Hide the label of every 2nd dataset. return null to hide the grid line too
-                    //return index % 2 === 0 ? dataLabel : '';
+                    }
+                    //Hide the label of every 2nd dataset. return null to hide the grid line too
+                    return index % 2 === 0 ? dataLabel : '';*/
                 }],
                 yAxes: [{
                     display: true,
@@ -114,7 +148,7 @@ function barChartSelectedCountry(confCanvas, recCanvas, deaCanvas, confLabel, re
                 lineHeight: 1,
                 padding: 8,
                 fontColor: '#69C8C8',
-                text: 'Confirmed'
+                text: label
             },
             legend: {
                 display: true,
@@ -129,85 +163,47 @@ function barChartSelectedCountry(confCanvas, recCanvas, deaCanvas, confLabel, re
             }
         }
     });
+}
 
-    const ctxRecovered = recCanvas.getContext('2d');
-    const dataRecovered = new Chart(ctxRecovered, {
-        type: 'bar',
+/*Bar chart for selected country with all values (confirmed, recovered, deaths)*/
+function barChartAll(canvas, dates, label, confirmed, recovered, deaths) {
+    barChart = new Chart(canvas, {
+        type: typeChart,
         data: {
             labels: dates,
             datasets: [{
-                label: recLabel,
-                backgroundColor: 'rgba(167,234,122,0.6)',
+                label: 'Confirmed',
+                backgroundColor: 'rgb(147,190,234, 0.6)',
+                borderColor: 'rgb(13,23,234)',
+                hoverBackgroundColor: 'rgb(147,190,234, 0.7)',
                 borderWidth: 1,
+                fill: false,
+                pointRadius: 3,
+                barThickness: 8,
+                maxBarThickness: 10,
+                data: confirmed
+            }, {
+                label: 'Recovered',
+                backgroundColor: 'rgba(167,234,122,0.6)',
                 borderColor: 'rgb(9,234,14)',
                 hoverBackgroundColor: 'rgba(187,234,109,0.7)',
-                barPercentage: 0.9,
-                barThickness: 10,
-                maxBarThickness: 15,
-                data: recList
-            }]
-        },
-        options: {
-            responsive: true,
-            responsiveAnimationDuration: 0,
-            maintainAspectRatio: false,
-            aspectRatio: 0.9,
-            onResize: null,
-            scales: {
-                xAxes: [{
-                    display: true,
-                    gridLines: {
-                        display: true
-                    },
-                    ticks: {
-                        autoSkip: true,
-                        maxTicksLimit: 25
-                    }
-                }],
-                yAxes: [{
-                    display: true,
-                    gridLines: {
-                        display: true
-                    }
-                }]
-            },
-            title: {
-                display: true,
-                fontSize: 20,
-                lineHeight: 1,
-                padding: 8,
-                fontColor: '#69C8C8',
-                text: 'Recovered'
-            },
-            legend: {
-                display: true,
-                position: 'bottom',
-                align: 'center',
-                labels: {
-                    fontColor: 'rgb(000, 000, 000)',
-                    fontSize: 12,
-                    boxWidth: 35,
-                    padding: 8
-                }
-            }
-        }
-    });
-
-    const ctxDeaths = deaCanvas.getContext('2d');
-    const dataDeaths = new Chart(ctxDeaths, {
-        type: 'bar',
-        data: {
-            labels: dates,
-            datasets: [{
-                label: deaLabel,
-                backgroundColor: 'rgba(234,127,121,0.6)',
                 borderWidth: 1,
+                fill: false,
+                pointRadius: 3,
+                barThickness: 8,
+                maxBarThickness: 10,
+                data: recovered
+            }, {
+                label: 'Deaths',
+                backgroundColor: 'rgba(234,127,121,0.6)',
                 borderColor: 'rgb(234,3,12)',
                 hoverBackgroundColor: 'rgba(234,81,77,0.7)',
-                barPercentage: 0.9,
-                barThickness: 10,
-                maxBarThickness: 15,
-                data: deaList
+                borderWidth: 1,
+                fill: false,
+                pointRadius: 3,
+                barThickness: 8,
+                maxBarThickness: 10,
+                data: deaths
             }]
         },
         options: {
@@ -219,16 +215,18 @@ function barChartSelectedCountry(confCanvas, recCanvas, deaCanvas, confLabel, re
             scales: {
                 xAxes: [{
                     display: true,
+                    stacked: booleanStacked,
                     gridLines: {
                         display: true
                     },
                     ticks: {
                         autoSkip: true,
-                        maxTicksLimit: 25
+                        maxTicksLimit: 17
                     }
                 }],
                 yAxes: [{
                     display: true,
+                    stacked: booleanStacked,
                     gridLines: {
                         display: true
                     }
@@ -240,7 +238,7 @@ function barChartSelectedCountry(confCanvas, recCanvas, deaCanvas, confLabel, re
                 lineHeight: 1,
                 padding: 8,
                 fontColor: '#69C8C8',
-                text: 'Deaths'
+                text: label
             },
             legend: {
                 display: true,
@@ -259,11 +257,11 @@ function barChartSelectedCountry(confCanvas, recCanvas, deaCanvas, confLabel, re
 
 let stackedBoolean = false;
 let charType = 'line';
-let barChart;
+let chartToggle;
 
 /*Toggle line and bar chart*/
 function change(newType, toggleCharts, dates, confirmed, recovered, deaths, countryToggle) {
-    barChart.destroy();
+    chartToggle.destroy();
 
     if (newType === 'bar') {
         charType = newType;
@@ -274,13 +272,11 @@ function change(newType, toggleCharts, dates, confirmed, recovered, deaths, coun
         stackedBoolean = false;
         toggleChartTypes(toggleCharts, dates, confirmed, recovered, deaths, countryToggle);
     }
-};
+}
 
 /*Line or bar chart*/
 function toggleChartTypes(toggleCharts, dates, confirmed, recovered, deaths, countryToggle) {
-
-    const ctxToggle = toggleCharts;
-    barChart = new Chart(ctxToggle, {
+    chartToggle = new Chart(toggleCharts, {
         type: charType,
         data: {
             labels: dates,
@@ -331,7 +327,7 @@ function toggleChartTypes(toggleCharts, dates, confirmed, recovered, deaths, cou
                     },
                     ticks: {
                         autoSkip: true,
-                        maxTicksLimit: 20
+                        maxTicksLimit: 17
                     }
                 }],
                 yAxes: [{
@@ -360,6 +356,67 @@ function toggleChartTypes(toggleCharts, dates, confirmed, recovered, deaths, cou
                     boxWidth: 35,
                     padding: 8
                 }
+            }
+        }
+    });
+}
+
+/*Sir Model*/
+function sirModelChart(sirModelCanvas, sus, inf, rec) {
+    let days = [];
+    for (let i = 1; i <= sus.length; i++) {
+        days.push('Day ' + i);
+    }
+
+    let myChart = new Chart(sirModelCanvas, {
+        type: 'line',
+        data: {
+            labels: days,
+            datasets: [{
+                label: 'Susceptible',
+                borderColor: 'rgb(65, 133, 234, 0.6)',
+                fill: false,
+                pointRadius: 5,
+                data: sus
+            }, {
+                label: 'Infected',
+                borderColor: 'rgb(250, 2, 6, 0.6)',
+                fill: false,
+                pointRadius: 5,
+                data: inf
+            }, {
+                label: 'Recovered',
+                borderColor: 'rgb(27, 205, 4, 0.6)',
+                fill: false,
+                pointRadius: 5,
+                data: rec,
+                showLine: true
+            }]
+        },
+        options: {
+            responsive: true,
+            responsiveAnimationDuration: 0,
+            maintainAspectRatio: false,
+            aspectRatio: 0.9,
+            onResize: null,
+            tooltips: {
+                mode: 'point'
+            },
+            legend: {
+                display: true,
+                position: 'bottom',
+                align: 'center',
+                labels: {
+                    boxWidth: 40,
+                    fontColor: 'rgb(000, 000, 000)'
+                }
+            },
+            title: {
+                display: true,
+                fontSize: 20,
+                padding: 15,
+                fontColor: '#69C8C8',
+                text: 'SIR Model'
             }
         }
     });
