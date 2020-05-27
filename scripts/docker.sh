8dbb5b8c53beea22d6930499261d8c7806ba9b84
Antonio Troiano <antonio.troiano@valtech.com>
2020-05-27 20:50:54 +0200

    Update build.jj and docker.sh

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
-cd ..
-mvn clean
-mvn package
 docker stop statistics
 docker rm statistics
-docker build -f DockerfileLocal -t statistics .
+docker build -f Dockerfile -t statistics .
 docker run --name statistics -d -p 8080:8080 statistics
